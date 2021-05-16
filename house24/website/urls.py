7f9e07d628d250e9f4f9661c4ed85f7c10cafb41
chenger1 <exs2199@gmail.com>
2021-05-16 14:35:07 +0300

    Add "About Us" page template

--- house24/website/urls.py
+++ house24/website/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.staticfiles.urls import static
 from django.conf import settings
 
-from website.views.pages import MainPageView
+from website.views.pages import MainPageView, AboutPageView
 
 
 app_name = 'website'
@@ -10,4 +10,5 @@ app_name = 'website'
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='main_page_view'),
+    path('about/', AboutPageView.as_view(), name='about_page_view'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- house24/website/views/pages.py
+++ house24/website/views/pages.py
@@ -1,8 +1,13 @@
-from db.models.pages import MainPage
+from db.models.pages import MainPage, AboutPage
 
-from website.views.singleton_page_mxin import SingletonView
+from website.views.singleton_page_mixin import SingletonView
 
 
 class MainPageView(SingletonView):
     model = MainPage
     template_name = 'pages/main_page.html'
+
+
+class AboutPageView(SingletonView):
+    model = AboutPage
+    template_name = 'pages/about.html'
