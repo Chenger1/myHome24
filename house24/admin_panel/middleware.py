25f2879ff22fbcba09a6e08c87f3a929b85e8c45
chenger1 <exs2199@gmail.com>
2021-05-22 19:27:37 +0300

    Add checking for role perms

--- house24/admin_panel/middleware.py
+++ house24/admin_panel/middleware.py
@@ -11,21 +11,21 @@ class AdminCheckMiddleware(MiddlewareMixin):
         if 'admin' in request.path:
             if user.is_authenticated:
                 if request.path == '/admin/' and not request.user.role.statistic:
-                    raise Http404()
+                    return redirect(request.user.role.get_available_url_pattern_by_role())
                 elif 'services/index/' in request.path and not request.user.role.services:
-                    raise Http404()
+                    return redirect(request.user.role.get_available_url_pattern_by_role())
                 elif 'tariff/index/' in request.path and not request.user.role.tariffs:
-                    raise Http404()
+                    return redirect(request.user.role.get_available_url_pattern_by_role())
                 elif 'roles/index/' in request.path and not request.user.role.roles:
-                    raise Http404()
+                    return redirect(request.user.role.get_available_url_pattern_by_role())
                 elif 'user-admin/index/' in request.path and not request.user.role.users:
-                    raise Http404()
+                    return redirect(request.user.role.get_available_url_pattern_by_role())
                 elif 'credentials/index/' in request.path and not request.user.role.credentials:
-                    raise Http404()
+                    return redirect(request.user.role.get_available_url_pattern_by_role())
                 elif 'main_page' in request.path or 'about' in request.path or 'services' in request.path \
                      or 'tariffs' in request.path or 'contacts' in request.path:
                     if not request.user.role.site_control:
-                        raise Http404()
+                        return redirect(request.user.role.get_available_url_pattern_by_role())
             elif 'login/' not in request.path:
                 return redirect('website:admin_site_login_view')
         return self.get_response(request)
