77f9ee75e5c6ac3d79f33fc9bd6fbbbc9b2f8235
chenger1 <exs2199@gmail.com>
2021-05-22 11:53:11 +0300

    Add View for Roles

---
+++ house24/admin_panel/views/user/user_views.py
@@ -0,0 +1,26 @@
+from django.views.generic import View
+from django.shortcuts import render, redirect
+
+from db.models.user import Role
+
+from admin_panel.forms.user_forms import RoleFormSet
+
+
+class UpdateRolesVies(View):
+    model = Role
+    template_name = 'user/roles_list.html'
+    context_object_name = 'roles'
+    redirect_url = 'admin_panel:list_roles_admin'
+
+    def get(self, request):
+        formset = RoleFormSet()
+        return render(request, self.template_name, context={'formset': formset})
+
+    def post(self, request):
+        formset = RoleFormSet(request.POST)
+        if formset.is_valid():
+            for form in formset:
+                form.save()
+            return redirect('admin_panel:list_roles_admin')
+        else:
+            return render(request, self.template_name, context={'formset': formset})
