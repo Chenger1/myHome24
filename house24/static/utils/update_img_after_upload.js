df3032cfbe2050503807a01c7d79c45889eca26c
chenger1 <exs2199@gmail.com>
2021-05-24 20:22:23 +0300

    Update update_img_after_upload.js to maintain a lot wide range of structures

--- house24/static/utils/update_img_after_upload.js
+++ house24/static/utils/update_img_after_upload.js
@@ -1,9 +1,9 @@
-function setPreview(input, selector='div[class*=col]'){
+function setPreview(input, selector='div[class*=col]', image_selector='.preview_img'){
   if(input.files && input.files[0]){ // check if input widget uploads any files
     let reader = new FileReader(); // create new instance of FileReader object
 
     reader.onload = function(e){ // set function to onload handler to set new image preview
-      $(input).closest(selector).find('.preview_img').attr('src', e.target.result);
+      $(input).closest(selector).find(image_selector).attr('src', e.target.result);
       // fidn parent column div and search for closes image preview
     };
 
