1ab43e18736c28d479ba64adb0a046889fb3a884
chenger1 <exs2199@gmail.com>
2021-05-25 09:27:41 +0300

    Add View to UpdateHouse

--- house24/admin_panel/urls.py
+++ house24/admin_panel/urls.py
@@ -14,7 +14,7 @@ from admin_panel.views.options.tariff_option_views import (ListTariff, CreateTar
 from admin_panel.views.user.user_views import (UpdateRolesView, ListUsersView, CreateAdminUser, UpdateAdminUser,
                                                DeleteAdminUser, ListOwnerView, CreateOwnerUser, UpdateOwnerUser,
                                                DeleteOwnerUser)
-from admin_panel.views.house_views import ListHousesView, CreateHouseView, GetUserRole
+from admin_panel.views.house_views import ListHousesView, CreateHouseView, UpdateHouseView, GetUserRole
 
 
 app_name = 'admin_panel'
@@ -78,5 +78,6 @@ urlpatterns = [
     # HOUSES
     path('houses/index/', ListHousesView.as_view(), name='list_houses_admin'),
     path('houses/index/create_house/', CreateHouseView.as_view(), name='create_house_admin'),
+    path('houses/index/update_house/<int:pk>/', UpdateHouseView.as_view(), name='update_house_admin'),
     path('houses/index/get_user_role/', GetUserRole.as_view(), name='get_user_role'),
 ]
--- house24/admin_panel/views/house_views.py
+++ house24/admin_panel/views/house_views.py
@@ -29,7 +29,7 @@ class CreateHouseView(View):
         section_formset = SectionFormset()
         floor_formset = FloorFormset()
         user_formset = UserFormset(prefix='users')
-
+        user_formset.forms = []
         return render(request, self.template_name, context={'form': form,
                                                             'section_formset': section_formset,
                                                             'floor_formset': floor_formset,
@@ -42,12 +42,46 @@ class CreateHouseView(View):
         user_formset = UserFormset(request.POST, prefix='users')
         if form.is_valid() and section_formset.is_valid() and floor_formset.is_valid() and user_formset.is_valid():
             obj = form.save()
-            section_formset.instance = floor_formset.instance = obj
+            section_formset.instance = floor_formset.instance = user_formset.instance = obj
             section_formset.save()
             floor_formset.save()
-            for user in user_formset.cleaned_data:
-                #  This formset contain obj to many-to-many fields
-                obj.users.add(user['user'])
+            user_formset.save()
+            return redirect(self.redirect_url)
+        else:
+            return render(request, self.template_name, context={'form': form,
+                                                                'section_formset': section_formset,
+                                                                'floor_formset': floor_formset,
+                                                                'user_formset': user_formset})
+
+
+class UpdateHouseView(View):
+    model = House
+    template_name = 'houses/create_house_admin.html'
+    redirect_url = 'admin_panel:list_houses_admin'
+
+    def get(self, request, pk):
+        inst = get_object_or_404(self.model, pk=pk)
+        form = CreateHouseForm(instance=inst)
+        section_formset = SectionFormset(instance=inst)
+        floor_formset = FloorFormset(instance=inst)
+        user_formset = UserFormset(instance=inst, prefix='users')
+
+        return render(request, self.template_name, context={'form': form,
+                                                            'section_formset': section_formset,
+                                                            'floor_formset': floor_formset,
+                                                            'user_formset': user_formset})
+
+    def post(self, request, pk):
+        inst = get_object_or_404(self.model, pk=pk)
+        form = CreateHouseForm(request.POST, request.FILES, instance=inst)
+        section_formset = SectionFormset(request.POST, instance=inst)
+        floor_formset = FloorFormset(request.POST, instance=inst)
+        user_formset = UserFormset(request.POST, prefix='users', instance=inst)
+        if form.is_valid() and section_formset.is_valid() and floor_formset.is_valid() and user_formset.is_valid():
+            obj = form.save()
+            section_formset.save()
+            floor_formset.save()
+            user_formset.save()
             return redirect(self.redirect_url)
         else:
             return render(request, self.template_name, context={'form': form,
