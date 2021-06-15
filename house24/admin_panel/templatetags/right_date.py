26ef7b69e3c915898344bef04fb5459ac608f6ec
chenger1 <exs2199@gmail.com>
2021-06-16 00:50:10 +0300

    Fix wrong time format in master request detail page

--- house24/admin_panel/templatetags/right_date.py
+++ house24/admin_panel/templatetags/right_date.py
@@ -9,3 +9,8 @@ register = template.Library()
 @register.filter(name='valid_date_format')
 def valid_date_format(value):
     return datetime.datetime.strftime(value, '%Y-%m-%d')
+
+
+@register.filter(name='valid_time_format')
+def valid_time_format(value):
+    return datetime.time.strftime(value, '%H:%M')
