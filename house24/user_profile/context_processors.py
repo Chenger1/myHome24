0cfa6d528df4259714c8a2e2bdbf0c15dca54bb5
Chenger1 <exs2199@gmail.com>
2021-06-21 22:41:18 +0300

    Add context_processor for user profile base html

---
+++ house24/user_profile/context_processors.py
@@ -0,0 +1,20 @@
+from db.models.house import Message, Section, Floor, House
+
+
+def add_client_info_to_template(request):
+    if not request.user.is_staff:
+        flats = request.user.flats.all()
+        houses = House.objects.filter(flats__in=flats)
+        sections = Section.objects.filter(flats__in=flats)
+        floors = Floor.objects.filter(flats__in=flats)
+        messages = Message.objects.filter(house__in=houses,
+                                          section__in=sections,
+                                          floor__in=floors,
+                                          flat__in=flats,
+                                          with_debt=request.user.has_debt)
+        return {
+            'houses': houses,
+            'flats': flats,
+            'messages': messages,
+            'messages_count': messages.count()
+        }
