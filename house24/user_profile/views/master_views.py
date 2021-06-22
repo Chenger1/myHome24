75891e204dd645691030dc871bcfc438a92d9ece
Chenger1 <exs2199@gmail.com>
2021-06-22 19:49:38 +0300

    Add mixin for paginated list

--- house24/user_profile/views/master_views.py
+++ house24/user_profile/views/master_views.py
@@ -1,35 +1,21 @@
 from django.views.generic import View
 from django.shortcuts import render, redirect, get_object_or_404
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from db.models.house import MasterRequest
 
 from user_profile.forms.client_forms import CreateMasterRequest
+from user_profile.views.mixin import ListPaginatedQuery
 
 
-class ListClientMasterRequestView(View):
+class ListClientMasterRequestView(ListPaginatedQuery):
     model = MasterRequest
     template_name = 'master_requests/list_client_master_requests.html'
-    paginate_by = 15
 
     def get(self, request, pk):
         instances = self.model.objects.filter(owner=pk)
         page = request.GET.get('page')
         return render(request, self.template_name, context={'instances': self.get_paginated_query(instances,
                                                                                                   page)})
-
-    def get_paginated_query(self, query, current_page):
-        paginator = Paginator(query, self.paginate_by)
-        page = current_page
-
-        try:
-            queryset = paginator.page(page)
-        except PageNotAnInteger:
-            queryset = paginator.page(1)
-        except EmptyPage:
-            queryset = paginator.page(paginator.num_pages)
-
-        return queryset
 
 
 class CreateClientMasterRequestView(View):
---
+++ house24/user_profile/views/mixin.py
@@ -0,0 +1,21 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views.generic import View
+
+
+class ListPaginatedQuery(View):
+    model = None
+    template_name = None
+    paginate_by = 15
+
+    def get_paginated_query(self, query, current_page):
+        paginator = Paginator(query, self.paginate_by)
+        page = current_page
+
+        try:
+            queryset = paginator.page(page)
+        except PageNotAnInteger:
+            queryset = paginator.page(1)
+        except EmptyPage:
+            queryset = paginator.page(paginator.num_pages)
+
+        return queryset
