0200e20feb9cad67ea9364f3d8b907fe9989d356
Chenger1 <exs2199@gmail.com>
2021-06-28 11:58:59 +0300

    Update settings

--- house24/house24/settings.py
+++ house24/house24/settings.py
@@ -90,21 +90,13 @@ WSGI_APPLICATION = 'house24.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': os.environ.get('POSTGRES_ENGINE', 'django.db.backends.sqlite3'),
-    #     'NAME': os.environ.get('POSTGRES_DB', os.path.join(BASE_DIR, "db.sqlite3")),
-    #     'USER': os.environ.get('POSTGRES_USER', 'user'),
-    #     'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'password'),
-    #     'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
-    #     'PORT': os.environ.get('POSTGRES_POR', '5432')
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'house_db',
-        'USER': 'nitron',
-        'PASSWORD': '0660',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': os.environ.get('POSTGRES_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.environ.get('POSTGRES_DB', os.path.join(BASE_DIR, "house_db")),
+        'USER': os.environ.get('POSTGRES_USER', 'nitron'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '0660'), # temporary user and password. Doesnt important absolutely
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_POR', '5432')
     }
 }
 
