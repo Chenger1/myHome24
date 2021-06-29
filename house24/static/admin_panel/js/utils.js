4102c9bfe1b7e38eeed7bc1238f366818d00c75e
Chenger1 <exs2199@gmail.com>
2021-06-29 13:57:19 +0300

    Fix submit for search form

--- house24/static/admin_panel/js/utils.js
+++ house24/static/admin_panel/js/utils.js
@@ -10,17 +10,13 @@
 
     function submit_form_after_keypress(){
         $('.form-control').each(function(){
-            $(this).keydown(function(event) {
-                if(event.which == 13){
-                    $('form').submit();
-                }
-            });
+            $(this).on('change', function(){
+                $('form').submit();
+            })
        })
         $('.select2-selection').each(function(){
-            $(this).on('keyup', function(e){
-                if(e.keyCode === 13){
-                    $('form').submit();
-                }
+            $(this).on('change', function(e){
+                $('form').submit();
             })
         })
     }
